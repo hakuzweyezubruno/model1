25954b9482aefd85c9c9bef873a1dfbe98f766c5
hakuzweyezubruno <hakunzweyezubruno@gmail.com>
2026-05-21 13:30:13 +0200

    adding model practice

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn= document.querySelector(".open-model");
+const overLay=document.querySelector(".overlay");
+const model=document.querySelector(".model");
+const closeBtn=document.querySelector(".close-model-btn");
+btn.addEventListener("click",function (){
+    overLay.classList.remove("hidden");
+    model.classList.remove("hidden");
+})
+// closeBtn.addEventListener("click",function removeOveray() {
+//      overLay.classList.toggle("hidden");
+//     model.classList.toggle("hidden");
+// })
+// overLay.addEventListener("click", function removeOveray(){
+//     // overLay.classList.toggle("hidden");
+//     // model.classList.toggle("hidden");
+// })
+// overLay.addEventListener("click",removeOveray())
+overLay.addEventListener("click",close);
+closeBtn.addEventListener("click",close);
+function close() {
+    overLay.classList.toggle("hidden");
+    model.classList.toggle("hidden");
+}
